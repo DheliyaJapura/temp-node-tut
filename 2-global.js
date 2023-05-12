cdc24902e2f9ab271a5caf7dcbeb301dcc933358
DheliyaJapur <dhelaisuranga123@gmail.com>
2023-05-12 19:42:16 +0530

    first commit

---
+++ 2-global.js
@@ -0,0 +1,5 @@
+//GLOBALS
+console.log(__dirname)
+setInterval(() => {
+    console.log("hell wrld");
+}, 1000)
---
+++ 4-names.js
@@ -0,0 +1,7 @@
+//LOCAL
+const secret = "Super Secret"
+//SHARE
+const john = 'john'
+const peter = 'peter'
+
+module.exports = { john, peter }
